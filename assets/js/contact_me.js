266f561ae0bb4e91600869e7e5941ff90993fab4
Guillaume Dumas <deep@introspection.eu>
2020-11-29 22:27:38 -0500

    Update contact

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -10,7 +10,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "{{ site.email }}";
+	  var url = "https://formspree.io/f/mgepllgp";
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
