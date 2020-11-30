810b94518562ba146e28cbd8fe628a314cadd65b
Guillaume Dumas <deep@introspection.eu>
2020-11-29 23:04:39 -0500

    Update contact

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -10,10 +10,10 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
+	    var url = "https://formspree.io/f/mgepllgp";
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
+      var upload = $("input#upload").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -27,9 +27,9 @@ $(function() {
         type: "POST",
         data: {
           name: name,
-          phone: phone,
+          upload: upload,
           email: email,
-          message: message
+          message: message,
         },
         cache: false,
         
@@ -48,11 +48,11 @@ $(function() {
 		
         error: function() {
           // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that our mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
+          $('#success > .alert-success').append($("<strong>").text("Thanks " + firstName + "! Form submitted successfully."));
+          $('#success > .alert-success').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
