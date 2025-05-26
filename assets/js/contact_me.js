f65a29326c731c324ca2d4e61a2dfd8ee678f5c5
avicndugu <37213663+avicndugu@users.noreply.github.com>
2025-05-26 10:39:38 +0300

    Clean up white space in the diff

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -10,7 +10,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
+	    var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
       var name = $("input#name").val();
       var username = $("input#username").val();
       var email = $("input#email").val();
@@ -24,49 +24,50 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       if(username === ''){ // Disable's form submit if hidden input(username) has been filled
-        $.ajax({
-          url: url,
-          type: "POST",
-          dataType: "json",
-          data: {
-            name: name,
-            phone: phone,
-            email: email,
-            message: message
-          },
-          cache: false,
+      $.ajax({
 
-    	  success: function() {
-            // Success message
-            $('#success').html("<div class='alert alert-success'>");
-            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-            $('#success > .alert-success')
-              .append("<strong>Your message has been sent. </strong>");
-            $('#success > .alert-success')
-              .append('</div>');
-              //clear all fields
-            $('#contactForm').trigger("reset");
-          },
+        url: url,
+        type: "POST",
+        dataType: "json",
+        data: {
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
+        },
+        cache: false,
 
-          error: function() {
-            // Fail message
-            $('#success').html("<div class='alert alert-danger'>");
-            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-            $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-            $('#success > .alert-danger').append('</div>');
+      success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
             //clear all fields
-            $('#contactForm').trigger("reset");
-          },
+          $('#contactForm').trigger("reset");
+        },
 
-          complete: function() {
-            setTimeout(function() {
-              $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-            }, 1000);
-          }
-        });
-      }
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    }
     },
     filter: function() {
       return $(this).is(":visible");
